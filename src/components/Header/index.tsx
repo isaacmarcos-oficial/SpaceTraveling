0459df7faaf15a91d0317ee77f851a3370df00c8
Isaac Marcos <isaacmarcos@scdesign.com.br>
2021-12-14 19:10:11 -0300

    first commit

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+import styles from './header.module.scss'
+
+export default function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <div>
+        <Link href="/">
+          <img src="images/logo.svg" alt="logo" />
+        </Link>
+      </div>
+    </header>
+  );
+}
