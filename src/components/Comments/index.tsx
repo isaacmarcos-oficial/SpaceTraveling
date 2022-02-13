7cd9df069b4cb45b86cc96e11e39ad24af3c97a2
Isaac Marcos <isaacmarcos@scdesign.com.br>
2022-02-12 21:47:13 -0300

    config comments

--- src/components/Comments/index.tsx
+++ src/components/Comments/index.tsx
@@ -7,7 +7,7 @@ export default class Comments extends Component {
     script.setAttribute('src', 'https://utteranc.es/client.js');
     script.setAttribute('crossorigin', 'anonymous');
     script.setAttribute('async', 'true');
-    script.setAttribute('repo', 'GBDev13/space-traveling');
+    script.setAttribute('repo', 'isaacmarcos-oficial/SpaceTraveling');
     script.setAttribute('issue-term', 'pathname');
     script.setAttribute('theme', 'dark-blue');
     anchor.appendChild(script);
